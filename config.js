5ff815a84e467f99c3fba24b3c287e6c8aaacff7
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-10-16 21:47:21 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+        apiUrl: 'http://my.hepic.server/',
+        apiSess: 'http://my.hepic.server/api/v2/session',
+        apiUser: 'admin',
+        apiPass: 'password',
+        timeOut: 1800, // seconds
+        proxyHost: '0.0.0.0',
+        proxyPort: 8765
+};
+
+module.exports = config;
